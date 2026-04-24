24c6dbcec19b6070f163ec9bf46838119ab1ac8b
emiliaputri <emiliiaaputri@gmail.com>
2026-04-24 16:47:40 +0700

    fix: android-build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
 allprojects {
     repositories {
         google()
@@ -5,20 +8,21 @@ allprojects {
     }
 }
 
+// Custom build directory (opsional, tetap dipakai dari kode kamu)
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+// Atur build dir tiap subproject
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
+// Task clean
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
